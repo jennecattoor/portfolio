662eee782bc1a814186694b494f76253e7095070
Jenneeee <jenne@cattoor.net>
2022-09-17 20:55:45 +0200

    First commit, Gatsby starter files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Jenne Cattoor `,
+    siteUrl: `https://www.jennecattoor.com`,
+    description: `The portfolio of Jenne Cattoor, a multidisciplinary designer and front-end developer based in Belgium. Creating digital experiences and visual identities.`,
+    author: `Jenne Cattoor`,
+    keywords: `jenne cattoor, portfolio, designer, developer, freelance, ui, ux, front-end developer, digital experiences, visual identities`,
   },
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
